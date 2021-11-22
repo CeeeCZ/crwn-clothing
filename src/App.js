d4b074efd48fdc60a2a2065067008c431cacda5b
uhliko <ondrej.uhlik@vodafone.com>
2021-11-22 16:22:41 +0100

    header

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import HomePage from './pages/homepage/homepage.component'
 import ShopPage from './pages/shop/shop.component'
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Header from './components/header/header.component';
 
 const HatsPage = () => (
   <div>
@@ -13,6 +14,7 @@ function App() {
   return (
     <div>
       <BrowserRouter>
+        <Header />
         <Switch>
           <Route exact path='/' component={HomePage} />
           <Route path='/hats' component={HatsPage} />
